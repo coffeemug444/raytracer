39c13745d124bdd19c18c84bc9c1d8a7b00f34dc
Ben Slattery <ben@benslattery.com>
2022-10-24 01:17:55 +1300

    introduced camera class

--- fragment.glsl
+++ fragment.glsl
@@ -1,7 +1,8 @@
 #version 460 core
+in vec3 rayDir;
 out vec4 FragColor;
 
 void main()
 {
-   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+   FragColor = vec4(0.0f, rayDir.y 0.2f, 1.0f);
 }
--- main.cpp
+++ main.cpp
@@ -1,15 +1,26 @@
 #include "glad.h"
 #include <GLFW/glfw3.h>
 #include "shader.h"
+#include "camera.h"
 
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
+void keyCallback(GLFWwindow* window, int key, int, int action, int);
+void mouse_callback(GLFWwindow*, double xpos, double ypos);
 
-// settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
+const unsigned int SCR_WIDTH = 1280;
+const unsigned int SCR_HEIGHT = 720;
+float fov = 1.5708 / 2.f;
+
+Camera camera(glm::vec3(0.0f, 0.0f, 1.0f));
+float lastX, lastY;
+bool firstMouse = true;
+
+// timing
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
 
 GLFWwindow* initWindow()
 {
@@ -18,9 +29,8 @@ GLFWwindow* initWindow()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-    glfwWindowHint( GLFW_DOUBLEBUFFER, GL_FALSE);
 
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Ray Tracer", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -28,6 +38,9 @@ GLFWwindow* initWindow()
         return nullptr;
     }
     glfwMakeContextCurrent(window);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetKeyCallback(window, keyCallback);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
@@ -76,7 +89,20 @@ int main()
 
     while (!glfwWindowShouldClose(window))
     {
+		float currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
         processInput(window);
+
+        
+		shaderProgram.setFloat("screenWidth", (float)SCR_WIDTH);
+		shaderProgram.setFloat("screenHeight", (float)SCR_HEIGHT);
+		shaderProgram.setFloat("fov", fov);
+		shaderProgram.setMat3("view", camera.GetViewMatrix());
+		shaderProgram.setVec3("camPos", camera.Position);
+
+
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
@@ -84,7 +110,7 @@ int main()
         glBindVertexArray(VAO); 
         glDrawArrays(GL_TRIANGLES, 0, 6);
 
-        glFlush();
+		glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
@@ -95,8 +121,52 @@ int main()
     return 0;
 }
 
+void mouse_callback(GLFWwindow*, double xpos, double ypos)
+{
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = (xpos - lastX);
+	float yoffset = (lastY - ypos); // reversed since y-coordinates go from bottom to top
+
+	lastX = xpos;
+	lastY = ypos;
+
+	camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
 void processInput(GLFWwindow *window)
 {
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.ProcessKeyboard(FORWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.ProcessKeyboard(BACKWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.ProcessKeyboard(LEFT, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.ProcessKeyboard(RIGHT, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_T) == GLFW_PRESS)
+		fov = fov * (deltaTime + 1);
+	if (glfwGetKey(window, GLFW_KEY_G) == GLFW_PRESS)
+		fov = fov / (deltaTime + 1);
+}
+
+void keyCallback(GLFWwindow* window, int key, int, int action, int)
+{
+    if ((key != GLFW_KEY_LEFT_CONTROL) || action != GLFW_PRESS) return;
+
+    if (glfwGetInputMode(window, GLFW_CURSOR) == GLFW_CURSOR_DISABLED) {
+        glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+        glfwSetCursorPosCallback(window, NULL);
+    } else {
+        glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+        firstMouse = true;
+        glfwSetCursorPosCallback(window, mouse_callback);
+    }
 }
